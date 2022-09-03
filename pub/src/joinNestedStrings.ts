3b7eae962f3d0335f3702d84cb2ca5ad3815ce10
corno <corno.astn.a@schraverus.com>
2022-09-03 14:33:47 +0200

    .

--- pub/src/joinNestedStrings.ts
+++ pub/src/joinNestedStrings.ts
@@ -2,7 +2,7 @@ import * as pt from "pareto-core-types"
 import * as pi from "pareto-core-internals"
 
 export function joinNestedStrings(
-    array: pt.Nested<string>,
+    array: pt.Nested<string>, separator: string,
 ): string {
-    return pi.flatten(array).join("")
+    return pi.flatten(array).join(separator)
 }
