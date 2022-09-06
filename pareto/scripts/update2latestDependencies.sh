166f00e82d74b8a4188426182e2e4e866168762c
corno <corno.astn.a@schraverus.com>
2022-09-06 14:18:33 +0200

    .

--- pareto/scripts/update2latestDependencies.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-scriptDir=`realpath $(dirname "$0")`
-rootDir="$scriptDir/../.."
-
-"$scriptDir/updatePackage.sh" dev
-"$scriptDir/updatePackage.sh" pub
-"$scriptDir/updatePackage.sh" test
